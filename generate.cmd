ba4438ca4549d3cbe196d4a75424c9f1620314f9
heiliuer <heiliuer@github.com>
2016-11-23 18:47:46 +0800

    fixed

--- generate.cmd
+++ generate.cmd
@@ -1,5 +1,5 @@
 @echo off
-mvn archetype:generate  -DarchetypeCatalog=local -DarchetypeGroupId=com.heiliuer.webapp -DarchetypeArtifactId=springmvc-quickstart -DarchetypeVersion=1.0 
-::-DgroupId=x:y -DartifactId=testMaven
-::-DinteractiveMode=false
+mvn archetype:generate -DgroupId=x:y -DartifactId=testMaven -DinteractiveMode=false  -DarchetypeCatalog=local -DarchetypeGroupId=com.heiliuer.webapp -DarchetypeArtifactId=springmvc-quickstart -DarchetypeVersion=1.0
+::
+::
 pause
